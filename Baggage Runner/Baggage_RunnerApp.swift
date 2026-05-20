891a87ffa32d2df13298cacedf72911a389d72ed
Hamad Siddiqui <hamad.siddiqui@gmail.com>
2026-05-20 16:36:20 -0600

    Initial Commit

---
+++ Baggage Runner/Baggage_RunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baggage_RunnerApp.swift
+//  Baggage Runner
+//
+//  Created by Hamad Siddiqui on 2026-05-20.
+//
+
+import SwiftUI
+
+@main
+struct Baggage_RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
